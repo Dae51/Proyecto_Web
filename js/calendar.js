688e070a47006c9a03baf0c7ad3c89316ec7b578
Dae51 <josue.portillo0123@gmail.com>
2025-10-03 21:51:16 -0600

    Calendario(final)

--- js/calendar.js
+++ js/calendar.js
@@ -1,23 +1,35 @@
 
+const TOOLTIP = ``
+
+
 /* Se declara el objeto calendario de la libreria FullCalendar*/
 document.addEventListener('DOMContentLoaded', function () {
-  const calendarEl = document.getElementById('calendar');
 
-  const calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    locale: 'es', // idioma español
-    events: [
-      {
-        title: 'Evento de prueba',
-        start: '2025-10-10',
-        end: '2025-10-12'
-      },
-      {
-        title: 'Reunión importante',
-        start: '2025-10-15T14:00:00'
-      }
-    ]
-  });
+    const calendarEl = document.getElementById('calendar');
 
-  calendar.render();
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+            start: '',
+            center: 'title',
+            end: 'today prevYear,prev,next,nextYear'
+        },
+/*         eventMouseEnter: function (info) {
+            var tooltip = new TOOLTIP
+        }, */
+        locale: 'es', // idioma español
+        events: [
+            {
+                title: 'Evento de prueba',
+                start: '2025-10-10',
+                end: '2025-10-12'
+            },
+            {
+                title: 'Live Aid',
+                start: '2025-10-14T14:00:00'
+            }
+        ]
+    });
+
+    calendar.render();
 });
